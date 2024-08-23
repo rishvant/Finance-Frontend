c2b059f6a4725e739390aa4dcec32917860084b6
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-08-23 18:42:59 +0530

    fix

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://finance-o02q.onrender.com/api";
 
 export const getOrders = async () => {
     try {
